a37ade61319832d4f83f00b41e55066823c06fd8
Maxim Chekulaev <thestarraccoon@gmail.com>
2023-03-15 01:14:09 +0400

    Request class and Service were added

--- app/Http/Controllers/Post/StoreController.php
+++ app/Http/Controllers/Post/StoreController.php
@@ -3,26 +3,18 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
 use App\Models\Post;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
-    public function __invoke(Request $request): RedirectResponse
+    public function __invoke(StoreRequest $request)
     {
-        $data = request()->validate([
-            'title' => 'required|string',
-            'content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
-        $tags = $data['tags'];
-        unset($data['tags']);
-        $post = Post::create($data);
+        $data = $request->validated();
 
-        $post->tags()->attach($tags);
+        $this->service->store($data);
 
         return redirect()->route('post.index');
     }
--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -3,26 +3,16 @@
 namespace App\Http\Controllers\Post;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\UpdateRequest;
 use App\Models\Post;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
-    public function __invoke(Post $post)
+    public function __invoke(UpdateRequest $request, Post $post)
     {
-        $data = request()->validate([
-            'title' => 'string',
-            'content' => 'string',
-            'image' => 'string',
-            'category_id' => '',
-            'tags' => '',
-        ]);
+        $data = request()->validate();
 
-        $tags = $data['tags'];
-        unset($data['tags']);
-
-        $post->update($data);
-
-        $post->tags()->sync($tags);
+        $this->service->update($data, $post);
 
         return redirect()->route('post.show', $post->id);
     }
